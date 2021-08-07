5140c3bb43ce4fa8996acabc615daa3a272b5c02
Rikito Taniguchi <rikiriki1238@gmail.com>
2021-08-07 18:37:35 +0900

    Add test: warn-unused-implicits

---
+++ input/src/main/scala/fix/fromscala2/warn-unused-implicits.scala
@@ -0,0 +1,39 @@
+/*
+rule = Unused
+*/
+package fix.fromscala2
+
+// scalac: -Ywarn-unused:implicits -Xfatal-warnings
+//
+
+trait InterFace {
+  /** Call something. */
+  def call(a: Int, b: String, c: Double)(implicit s: String): Int
+}
+
+trait BadAPI extends InterFace {
+  def f(a: Int,
+        b: String,
+        c: Double
+       )(implicit s: String): Int = { // assert: Unused
+    println(b + c)
+    a
+  }
+  @deprecated ("no warn in deprecated API", since="yesterday")
+  def g(a: Int,
+        b: String,
+        c: Double
+       )(implicit s: String): Int = { // assert: Unused
+    println(b + c)
+    a
+  }
+  override def call(a: Int,
+                    b: String,
+                    c: Double
+                   )(implicit s: String): Int = { // assert: Unused
+    println(b + c)
+    a
+  }
+
+  def i(implicit s: String, t: Int) = t // assert: Unused
+}
